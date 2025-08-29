6b5a91c6687254fb929665eb3fed5b85ced2263d
AnimeshLad31 <animeshlad31@gmail.com>
2025-08-30 02:19:54 +0530

    Add files via upload

---
+++ Python Class.py
@@ -0,0 +1,28 @@
+##Task 1
+
+a = input('First Value')
+a = int (a)
+b = input('Second Value')
+b = int (b)
+
+c = a + b
+print(c)
+
+d = a - b
+print(d)
+
+e = a * b
+print(e)
+
+f = a / b
+print(f)
+
+## Task 2
+
+A = input('First Name')
+A = str (A)
+B = input('Second Name')
+B = str (B)
+
+C = A + B
+print(C)
